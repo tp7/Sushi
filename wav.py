99bb50b51f38c4b8f8c97bb66a7b9d7f5a89895e
Victor Efimov <tp7ptr@gmail.com>
2014-05-17 22:47:19 +1100

    Add simple custom WAV reader implementation

--- wav.py
+++ wav.py
@@ -1,10 +1,76 @@
 import cv2
 import numpy as np
 from scipy.io import wavfile
+from chunk import Chunk
+import struct
+
+WAVE_FORMAT_PCM = 0x0001
+WAVE_FORMAT_EXTENSIBLE = 0xFFFE
+
+class WavFile(object):
+    def __init__(self, path):
+        super(WavFile, self).__init__()
+        self._file = open(path, 'rb')
+        try:
+            riff = Chunk(self._file, bigendian=False)
+            if riff.getname() != 'RIFF':
+                raise Exception('file does not start with RIFF id')
+            if riff.read(4) != 'WAVE':
+                raise Exception('not a WAVE file')
+
+            fmt = Chunk(self._file, bigendian=False)
+            if fmt.getname() != 'fmt ':
+                raise Exception('Invalid WAV header')
+            self._read_fmt_chunk(fmt)
+            fmt.skip()
+
+            self._data_chunk = Chunk(self._file, bigendian=False)
+            if self._data_chunk.getname() != 'data':
+                raise Exception('Invalid WAV header')
+            self.frames_count = self._data_chunk.chunksize // self.frame_size
+            self.data_start = self._file
+        except:
+            if self._file:
+                self._file.close()
+            raise
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        if self._file:
+            self._file.close()
+            self._file = None
+
+    def readframes(self, count):
+        if not count:
+            return ''
+        if self.sample_width == 2:
+            return self._data_chunk.read(count * self.frame_size)
+            # return struct.unpack('<{0}h'.format(count*self.channels_count), data)
+        elif self.sample_width == 3:
+            s = ''
+            for x in xrange(count):
+                frame = self._data_chunk.read(self.frame_size)
+                for c in xrange(0,3*self.channels_count, 3):
+                    s += '\0' + frame[c:(c+3)]
+
+            unpacked = struct.unpack('<{0}i'.format(count*self.channels_count), s)
+            return [x >> 8 for x in unpacked]
+
+
+    def _read_fmt_chunk(self, chunk):
+        wFormatTag, self.channels_count, self.framerate, dwAvgBytesPerSec, wBlockAlign = struct.unpack('<HHLLH', chunk.read(14))
+        if wFormatTag == WAVE_FORMAT_PCM or wFormatTag == WAVE_FORMAT_EXTENSIBLE: # ignore the rest
+            bits_per_sample = struct.unpack('<H', chunk.read(2))[0]
+            self.sample_width = (bits_per_sample + 7) // 8
+        else:
+            raise Exception('unknown format: {0}'.format(wFormatTag))
+        self.frame_size = self.channels_count * self.sample_width
 
 
 class WavStream(object):
-    def __init__(self, path, sample_rate = 12000, sample_type = 'float32'):
+    def __init__(self, path, sample_rate=12000, sample_type='float32'):
         rate, data = wavfile.read(path)
         data = np.array(data[:, 0], ndmin=2, dtype=np.uint16)
         self.samples_count = int(len(data[0]) / float(rate) * sample_rate)
@@ -46,6 +112,13 @@ class WavStream(object):
 
         return result[0][min_idx], start_time + (min_idx / float(self.sample_rate))
 
+
+# file = WavFile(r"H:\!Ongoing\shiftass\maou\[FFF] Hataraku Maou-sama! - 01 [DEF4B21E].wav")
+# file = WavFile(r"H:\[ANE] Koe de Oshigoto! [BDRip 1080p x264 FLAC]\[ANE] Koe de Oshigoto! - Creditless Opening [BDRip 1080p x264 FLAC].wav")
+# print(file.getsampwidth())
+# c = file.readframes(file.frames_count)
+# print(len(c))
+# print(file.readframes(10000))
 #
 # def write_wav(float_array, sample_rate, path):
 #     data = (float_array * 32768.0).astype(np.int16)
