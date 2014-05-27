51e6b68a5898db3019ceb18aae207101d28c3f0a
Victor Efimov <tp7ptr@gmail.com>
2014-05-27 20:46:30 +1100

    Add setup.py for building the app with py2exe

---
+++ setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+import os
+import py2exe
+import sys
+
+setup(
+    name='Sushi',
+    description='Automatic subtitle shifter based on audio',
+    version='0.1.0',
+    url='https://github.com/tp7/Sushi',
+    console=['sushi.py'],
+    license='MIT',
+    options={
+        'py2exe': {
+            'compressed': True,
+            'optimize': 2,
+            "excludes": ["translation", "Tkconstants", "Tkinter", "tcl", 'pyreadline', 'numpy.core.multiarray_tests',
+                         'numpy.core.operand_flag_tests', 'numpy.core.struct_ufunc_test', 'numpy.core.umath_tests',
+                         'numpy.core._dotblas'],
+            "dll_excludes": ['w9xpopen.exe', 'AVICAP32.dll', 'AVIFIL32.dll', 'MSACM32.dll', 'MSVFW32.dll'],
+        }
+    },
+    zipfile='lib/library.zip',
+    data_files=[
+        ('', ('readme.md', 'LICENSE')),
+        ('licenses', ('licenses/OpenCV.txt', 'licenses/SciPy.txt'))
+    ]
+)
+
+try:
+    dist_dir = next(sys.argv[i + 1].strip('"') for i, arg in enumerate(sys.argv) if arg.lower() == '-d')
+except StopIteration:
+    dist_dir = 'dist'
+
+# move our license to the right directory
+os.rename(os.path.join(dist_dir, 'LICENSE'), os.path.join(dist_dir, 'licenses', 'Sushi.txt'))
