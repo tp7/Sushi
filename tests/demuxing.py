a54cef9790cbd2e342480be39ce4190d35b41288
Victor Efimov <tp7ptr@gmail.com>
2015-01-04 22:28:41 +0300

    Add tests for ffmpeg invocation

--- tests/demuxing.py
+++ tests/demuxing.py
@@ -1,5 +1,16 @@
 import unittest
+import mock
+
 from demux import FFmpeg
+from common import SushiError
+
+
+def create_popen_mock():
+    popen_mock = mock.Mock()
+    process_mock = mock.Mock()
+    process_mock.configure_mock(**{'communicate.return_value': ('ouput', 'error')})
+    popen_mock.return_value = process_mock
+    return popen_mock
 
 
 class FFmpegTestCase(unittest.TestCase):
@@ -32,3 +43,36 @@ class FFmpegTestCase(unittest.TestCase):
         self.assertEqual(len(subs), 1)
         self.assertEqual(subs[0].id, 2)
         self.assertEqual(subs[0].title, 'English Subtitles')
+
+    @mock.patch('subprocess.Popen', new_callable=create_popen_mock)
+    def test_get_info_call_args(self, popen_mock):
+        FFmpeg.get_info('random_file.mkv')
+        self.assertEquals(popen_mock.call_args[0][0], ['ffmpeg', '-hide_banner', '-i', 'random_file.mkv'])
+
+    @mock.patch('subprocess.Popen', new_callable=create_popen_mock)
+    def test_get_info_fail_when_no_mmpeg(self, popen_mock):
+        popen_mock.return_value.communicate.side_effect = OSError(2, "ignored")
+        self.assertRaises(SushiError, lambda: FFmpeg.get_info('random.mkv'))
+
+    @mock.patch('subprocess.call')
+    def test_demux_file_call_args(self, call_mock):
+        FFmpeg.demux_file('random.mkv', audio_stream=0, audio_path='audio1.wav')
+        FFmpeg.demux_file('random.mkv', audio_stream=0, audio_path='audio2.wav', audio_rate=12000)
+        FFmpeg.demux_file('random.mkv', script_stream=0, script_path='subs1.ass')
+        FFmpeg.demux_file('random.mkv', video_stream=0, timecodes_path='tcs1.txt')
+
+        FFmpeg.demux_file('random.mkv', audio_stream=1, audio_path='audio0.wav', audio_rate=12000,
+                          script_stream=2, script_path='out0.ass', video_stream=0, timecodes_path='tcs0.txt')
+
+        call_mock.assert_any_call(['ffmpeg', '-hide_banner', '-i', 'random.mkv', '-y',
+                                   '-map', '0:0', '-ac', '1', '-acodec', 'pcm_s16le', 'audio1.wav'])
+        call_mock.assert_any_call(['ffmpeg', '-hide_banner', '-i', 'random.mkv', '-y',
+                                   '-map', '0:0', '-ar', '12000', '-ac', '1', '-acodec', 'pcm_s16le', 'audio2.wav'])
+        call_mock.assert_any_call(['ffmpeg', '-hide_banner', '-i', 'random.mkv', '-y',
+                                   '-map', '0:0', 'subs1.ass'])
+        call_mock.assert_any_call(['ffmpeg', '-hide_banner', '-i', 'random.mkv', '-y',
+                                   '-map', '0:0', '-f', 'mkvtimestamp_v2', 'tcs1.txt'])
+        call_mock.assert_any_call(['ffmpeg', '-hide_banner', '-i', 'random.mkv', '-y',
+                                   '-map', '0:1', '-ar', '12000', '-ac', '1', '-acodec', 'pcm_s16le', 'audio0.wav',
+                                   '-map', '0:2', 'out0.ass',
+                                   '-map', '0:0', '-f', 'mkvtimestamp_v2', 'tcs0.txt'])
